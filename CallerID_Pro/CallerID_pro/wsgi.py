b4e04c4f18b212446726dcbe674dc94c7e58a142
niteshmauryac7r <110805456+niteshmauryac7r@users.noreply.github.com>
2025-02-13 00:28:28 +0530

    Update wsgi.py

--- CallerID_Pro/CallerID_pro/wsgi.py
+++ CallerID_Pro/CallerID_pro/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Instahyre project.
+WSGI config for Caller_ID project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Instahyre.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CallerID_pro.settings')
 
 application = get_wsgi_application()
